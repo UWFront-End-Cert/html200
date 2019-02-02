cd56db6f3c5f0aac9b048460dcf9929ee64f07cc
SteveParris74 <steveqube@gmail.com>
2019-02-02 08:56:22 -0800

    third

---
+++ Lesson-3/assignment-1.js
@@ -0,0 +1,11 @@
+let arr = ['drums', 'bass', 'guitar', 'synthesizer', 'vocals'];
+
+console.log(arr);
+
+arr.push('violin');
+
+arr.splice(2, 1);
+
+let shoppingList = arr.join (',');
+
+console.log(shoppingList);
