305139e2392affb99b5576a95b4f785e139819e9
Alison Steck <alison.ruth830@gmail.com>
2019-02-04 15:19:59 -0800

    created code

---
+++ lesson-03/challenge01.js
@@ -0,0 +1,15 @@
+let arr = ['Blue', 'Red', 'Green', 'Yellow', 'Pink']
+
+console.log(arr);
+
+arr.push('Orange');
+
+console.log(arr);
+
+arr.splice(2, 1);
+
+console.log(arr);
+
+let colors = arr.join(', ');
+
+console.log(colors);
