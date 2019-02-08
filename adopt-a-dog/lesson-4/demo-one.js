8f2b230a63da93bd2505b308fee0fcd8ed8eea45
SteveParris74 <steveqube@gmail.com>
2019-02-07 22:15:53 -0800

    lesson4start

---
+++ adopt-a-dog/lesson-4/demo-one.js
@@ -0,0 +1,9 @@
+let numberSquared = prompt("Pick a Number to be Squared");
+
+console.log(numberSquared);
+
+let changedNumber = Number(numberSquared);
+
+changedNumber = numberSquared * numberSquared;
+
+alert(changedNumber);
