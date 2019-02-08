3c70d3b9e67d6a5ec52cd42ba3fde2edf6730422
Meghan Mullally <ms.mullally27@gmail.com>
2019-02-07 19:56:23 -0800

    added lesson 3 and 4

---
+++ lesson4-1/demo-one.js
@@ -0,0 +1,19 @@
+// let name = prompt('enter your name!');
+// let age = prompt('enter your age'); 
+
+// console.log(name); 
+// console.log(age); 
+
+// alert('Hi there ' + name); 
+
+
+// let drivingAge = Number(age);
+
+// drivingLength = age - 16;
+
+
+// alert(name + ' has been driving for ' + drivingLength + ' years')
+
+ 
+
+
