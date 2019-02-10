145ce6e8d69d720e149636b8aeb88df72e2a97b4
tom <tommy.surjadi@gmail.com>
2019-02-10 10:35:27 -0800

    Created alerts for each

--- dogAlert.js
+++ dogAlert.js
@@ -1,4 +1,5 @@
 function myFunction() {
-    document.getElementById('dogProfile').innerHTML = "My name is Murphy. I am siberian husky. Age: 2. . Loves running and bacon. Adoption cost $125";
+    document.getElementById('dogProfile').innerHTML = "My name is Murphy. I am border collie. Age: 2. . Loves running and bacon. Adoption cost $125";
 }
-//to display dog background.
+
+//to display dog profile.
