312f88d25c01402da82146c05f912ed3df8f5bb5
tom <tommy.surjadi@gmail.com>
2019-02-09 17:27:31 -0800

    Added details on dog profile

--- dogAlert.js
+++ dogAlert.js
@@ -1,4 +1,4 @@
 function myFunction() {
-    document.getElementById('dogProfile').innerHTML = "I am siberian husky. Age: 2. Potty-trained. Loves running and bacon.";
+    document.getElementById('dogProfile').innerHTML = "My name is Murphy. I am siberian husky. Age: 2. . Loves running and bacon. Adoption cost $125";
 }
 //to display dog background.
