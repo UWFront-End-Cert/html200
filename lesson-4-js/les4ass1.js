bd6a83cf026d031af0db17fd906fd41168443327
Jason DX <jxie7@uw.edu>
2019-02-09 21:56:24 -0800

    lesson 4

---
+++ lesson-4-js/les4ass1.js
@@ -0,0 +1,14 @@
+let number = prompt('Enter a number: ');
+let number1 = prompt('Enter a 2nd number: ');
+
+function square(number) {
+  return number * number
+}
+function square(number1) {
+  return number1 * number1
+}
+alert(square(number) + ' '+ square(number1));
+
+function myfunction() {
+  document.getElementById("demo").style.color = "blue";
+}
