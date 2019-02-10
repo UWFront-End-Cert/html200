55af07cb932ea0c893ee5fa942b80697b5413adb
tom <tommy.surjadi@gmail.com>
2019-02-09 17:23:59 -0800

    Added javascript on dog profile

--- dogAlert.js
+++ dogAlert.js
@@ -1,1 +1,4 @@
-alert('Dog name is xx');
+function myFunction() {
+    document.getElementById('dogProfile').innerHTML = "I am siberian husky. Age: 2. Potty-trained. Loves running and bacon.";
+}
+//to display dog background.
