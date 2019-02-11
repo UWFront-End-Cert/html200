805c3ca4391758514a446e4818b7bc237e2e5c54
Jason DX <jxie7@uw.edu>
2019-02-11 11:49:12 -0800

    css and some js

---
+++ index.js
@@ -0,0 +1,21 @@
+// function vistPage() {
+//     window.location.='dogs.html';
+// }
+
+function adoptMurphy() {
+    alert('Adopt: Murphy, $50, Chihuahua');
+}
+
+function adopt() {
+    alert('Adopt: Violet, $50, German Shepard');
+}
+
+let button = document.getElementById("adopt"),
+    count = 0;
+let button.onclick = function total() {
+    count += 1;
+    alert('Total: $', (count*50));
+}
+// function total() {
+//     alert('Total: $', (count*50));
+// }
