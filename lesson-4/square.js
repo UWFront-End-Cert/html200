464c18e42a373660ecc2d6d139d137baad908afb
HD <hhwkns@gmail.com>
2019-02-11 13:33:55 -0800

    L4 hw js

---
+++ lesson-4/square.js
@@ -0,0 +1,24 @@
+let number = prompt('Enter your favorite number');
+let squared = number * number;
+
+alert(number +  ' ' + 'squared is' +  ' ' + squared);
+
+
+
+
+// FUNCTION PRACTICE
+// function squared(numberToSquare) {
+//   number = numberToSquare * numberToSquare;
+//   return numberToSquare;
+// }
+// squared
+// alert(number + 'squared is' + squared(numberToSquare));
+//
+// squaredNumber(numberPrompt * numberPrompt);
+// consolelog(squaredNumber)
+//
+//
+// function doubler(numberToMultiply) {
+//   return numberToMultiply * 2;
+// }
+//   alert(doubler(number));
