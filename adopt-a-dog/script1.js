d94b83d99dcd69d171498d3d8bf82ec2c56c75f3
Duy-An Dam <andam85@gmail.com>
2019-02-10 22:39:09 -0800

    additions for HW4, added javascript file to handle onclick events for photos and buttons on index.html

---
+++ adopt-a-dog/script1.js
@@ -0,0 +1,54 @@
+function clickMurphy(){
+  let dogName = 'Murphy';
+  let dogBreed = 'Lab';
+  let adoptFee= '$123.45';
+
+  alert('Dog name: '+dogName+', Dog breed: '+dogBreed+', Adoption cost: '+adoptFee)
+}
+
+function clickPoppy(){
+  let dogName = 'Poppy';
+  let dogBreed = 'Maltese';
+  let adoptFee= '$123.45';
+
+  alert('Dog name: '+dogName+', Dog breed: '+dogBreed+', Adoption cost: '+adoptFee)
+}
+
+function clickJack(){
+  let dogName = 'Jack';
+  let dogBreed = 'Jack Russell Terrier';
+  let adoptFee= '$123.45';
+
+  alert('Dog name: '+dogName+', Dog breed: '+dogBreed+', Adoption cost: '+adoptFee)
+}
+
+function clickDuffy(){
+  let dogName = 'Duffy';
+  let dogBreed = 'Pitbull';
+  let adoptFee= '$123.45';
+
+  alert('Dog name: '+dogName+', Dog breed: '+dogBreed+', Adoption cost: '+adoptFee)
+}
+
+function clickLucas(){
+  let dogName = 'Lucas';
+  let dogBreed = 'Lab';
+  let adoptFee= '$123.45';
+
+  alert('Dog name: '+dogName+', Dog breed: '+dogBreed+', Adoption cost: '+adoptFee)
+}
+
+function clickJake(){
+  let dogName = 'Jake';
+  let dogBreed = 'Lab';
+  let adoptFee= '$123.45';
+
+  alert('Dog name: '+dogName+', Dog breed: '+dogBreed+', Adoption cost: '+adoptFee)
+}
+
+//total is an outer variable
+let total = 0;
+function runningTotal() {
+   total = total + 123.45;
+   alert('total cost ($): '+total);
+}
---
+++ hw4/script1.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Choose a number");
+let num2 = prompt("Choose your 2nd number")
+function numMulti(number1,number2){
+  return num1 * num2;
+}
+
+alert('the product of your numbers is: ' + numMulti(num1,num2));
