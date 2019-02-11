75d57ab7e80048720ea7f1fa3be762cdba5dcdbf
rfpete <rfpete@gmail.com>
2019-02-11 11:53:43 -0800

    added js to adopt-a-dog

---
+++ adopt-a-dog/dog-events.js
@@ -0,0 +1,36 @@
+function dogInformation(name, breed, fee) {
+  alert('This is ' + name + '\nBreed: ' + breed + '\nAdoption fee: ' + fee);
+}
+
+function calcTotal(fee) {
+  let total = 0;
+  let total = total + fee;
+  alert('Total adoption fees: ' + fee);
+}
+
+
+
+// var allButtons = document.querySelector('button');
+//
+// for (var i = 0; i < allButtons.length; i++) {
+//   allButtons[i].onclick = calcTotal;
+// }
+//
+//
+//
+// allButtons.onclick = calcTotal;
+
+
+
+
+
+
+// function cartTotal(fee) {
+//   // feeNumber = Number(fee);
+//   let total = 0;
+//   let total = total + fee;
+//   return total;
+//   alert('Total adoption fees: ' + total);
+// }
+//
+// console.log(total);
