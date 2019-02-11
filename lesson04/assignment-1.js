1bd0987fda510a3f4f936b5595f6982ad02e85e6
rfpete <rfpete@gmail.com>
2019-02-11 10:30:12 -0800

    finished assignment 1

--- lesson04/assignment-1.js
+++ lesson04/assignment-1.js
@@ -2,11 +2,9 @@ let enterNumber = prompt("Enter a number");
 
 let value = Number(enterNumber);
 
-function square(value) {
-  valueSquared = value * value;
-  return valueSquared;
+var btn = document.querySelector('button');
+
+btn.onclick = function() {
+  let valueSquared = value * value;
+  alert("Your number squared = " + valueSquared);
 }
-
-square(value);
-
-alert("Your number squared = " + valueSquared)
