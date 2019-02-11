e6e20d94ddc27a8107f5cea6a2eeab4f9d344886
SteveParris74 <steveqube@gmail.com>
2019-02-10 19:36:18 -0800

    progress

---
+++ adopt-a-dog/functions.js
@@ -0,0 +1,23 @@
+function murphyAlert() {
+
+  let murphyAlert = alert ('I am Murphy. A large mix, You can bring me home for $123.45')
+
+  console.log(murphyAlert);
+
+}
+
+function poppyAlert() {
+
+  let poppyAlert = alert ('I am Poppy. A Shih Tzu. You can bring me home for $123.45')
+
+  console.log(poppyAlert);
+
+}
+
+function jackAlert() {
+
+  let jackAlert = alert ('I am Jack. A young beagle. You can bring me home for $123.45')
+
+  console.log(jackAlert);
+
+}
