e76b1c217ee32fd4ca56cc07e4087e1daf589dc5
Jason DX <jxie7@uw.edu>
2019-02-11 21:49:52 -0800

    JS and CSS

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ function adopt() {
     alert('Adopt: Violet, $50, German Shepard');
 }
 
-let button = document.getElementById("adopt"),
-    count = 0;
-let button.onclick = function total() {
-    count += 1;
-    alert('Total: $', (count*50));
-}
-// function total() {
+// let button = document.getElementById("adopt"),
+//     count = 0;
+// let button.onclick = function total() {
+//     count += 1;
 //     alert('Total: $', (count*50));
 // }
+let sum = 0;
+function total() {
+    sum = sum + 50;
+    alert('Total: $' + sum);
+}
