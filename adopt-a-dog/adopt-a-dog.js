d4d2b94a0a05d8c78e054bf0babe750fb8bcc10f
Meghan Mullally <ms.mullally27@gmail.com>
2019-02-12 00:30:25 -0800

    added type, id, onclick to dogs.html and js connection

---
+++ adopt-a-dog/adopt-a-dog.js
@@ -0,0 +1,18 @@
+// Link JavaScript to your site.
+// Create click handlers that alerts the dog’s information when clicking on the dog’s photo.
+// You will need to pass in the params in the click handler of the dog’s name, breed and adoption fees.
+// Create click handlers on each dog’s button that will add to the total.
+// Alert this total.
+
+//Dog's Information - Name, Breed and Price
+
+let dogName;
+let dogBreed;
+let dogDescr;
+let dogPrice = 123.45;
+let checkoutTotal = 0;
+
+
+//Alert - dog info 
+
+alert('Name: ' + dogName + ' is ' + dogDescr + ' and ' + dogBreed + ' $ ' + dogPrice );
