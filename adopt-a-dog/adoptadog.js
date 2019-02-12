ec5a56886f82501ec10ac1ce217aa7b6bc7e3409
calinton <charlottelinton@users.noreply.github.com>
2019-02-12 07:21:20 -0800

    button event handlers added

--- adopt-a-dog/adoptadog.js
+++ adopt-a-dog/adoptadog.js
@@ -1,3 +1,7 @@
 function dogInfo(name, breed, cost) {
 alert('Name: ' + name + ', ' + 'Breed: ' + breed + ', ' + 'Cost: ' + '$' + cost);
 }
+
+function dogCost(cost) {
+  alert('The total is: ' + '$' + cost);
+}
