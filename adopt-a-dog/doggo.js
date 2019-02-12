29f38bc55d8bcad81079616d02915160961a5f44
Vu Nguyen <vunguyen@vus-macbook-pro.lan>
2019-02-12 00:12:12 -0800

    Completed assignment and ran through validator

---
+++ adopt-a-dog/doggo.js
@@ -0,0 +1,69 @@
+function clickOnMurphy(){
+  let dogName = 'Murphy';
+  let dogBreed = 'Mix';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: ' + dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+function clickOnPoppy(){
+  let dogName = 'Poppy';
+  let dogBreed = 'Shih Tzu';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: ' + dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+function clickOnJack(){
+  let dogName = 'Jack';
+  let dogBreed = 'Beagle';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: ' + dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+function clickOnDuffy(){
+  let dogName = 'Duffy';
+  let dogBreed = 'Pitbull';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: ' + dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+function clickOnLucas(){
+  let dogName = 'Lucas';
+  let dogBreed = 'Labrador';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: '+ dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+function clickOnJake(){
+  let dogName = 'Jake';
+  let dogBreed = 'Lab';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: ' + dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+function clickOnAngus(){
+  let dogName = 'Angus';
+  let dogBreed = 'Pug Mix';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: ' + dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+function clickOnViolet(){
+  let dogName = 'Violet';
+  let dogBreed = 'Chocolate Labrador';
+  let adoptionFee = '$123.45';
+
+  alert('Dog Name: ' + dogName + ', Dog Breed: ' + dogBreed + ', Adoption Cost: ' + adoptionFee)
+}
+
+let totalCost = 0;
+function addTotal() {
+   totalCost = totalCost + 123.45;
+   alert('Total adoption Cost (USD): ' + totalCost);
+}
