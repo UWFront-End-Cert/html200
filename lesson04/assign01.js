6faca4875859d7835c3c23854e0670cd1d0de0e6
Vu Nguyen <vunguyen@vus-macbook-pro.lan>
2019-02-11 17:23:40 -0800

    Edited some CSS and added lesson 4 assignment

---
+++ lesson04/assign01.js
@@ -0,0 +1,4 @@
+let number = prompt("Enter in number to square:"); //Ask for number
+let squaredNumber = number * number; //Square the number
+
+console.log("Your number squared is: " + squaredNumber); //Print squared number
---
+++ lesson04/demo-one.js
@@ -0,0 +1,26 @@
+// console.log("hi there");
+//
+// let name = prompt("Enter your name");
+// let lastName = prompt("Enter your last name")
+//
+// alert("Hi there " + name + " " + lastName);
+
+// ---
+//
+// let numberToDouble = prompt("Enter a number");
+//
+// let changedNumber = Number(numberToDouble);
+//
+// changedNumber = changedNumber * 2;
+//
+// alert(changedNumber);
+
+// ---
+//
+// let name = prompt("Enter in name");
+// let age = prompt("Enter in an age");
+// let driveCalculator = age - 16;
+//
+// alert(name + " has been driving for " + driveCalculator + " years!");
+//
+// ---
