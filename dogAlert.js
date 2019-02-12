cf50e65820df88e8adabea65b4b3d47f897a34d1
tom <tommy.surjadi@gmail.com>
2019-02-11 17:51:16 -0800

    Simplified header - moved logo+header from left-header div to main header box, to reduce #of div, to show 11791 previously hidden. thanks James. added profiledog 2,3,4. Making sure @page has script to js file

--- dogAlert.js
+++ dogAlert.js
@@ -1,15 +1,38 @@
 function showMessage() {
     alert('Welcome to adopt a dog!');
 }
-
 showMessage();
 
 //to display greetings
 
-function myProfile() {
-     alert('My Name is Murphy. Border Collie. $125');
 
+function myProfile() {
+     alert('My Name is Murphy. I am border collie. $123');
 }
 console.log();
 
-//to display dog name etc
+//to display dogprofile 1
+
+
+function myProfileTwo() {
+    alert('My Name is Poppy. I am husky. $123');
+}
+console.log();
+
+//to display dogprofile 2
+
+
+function myProfileThree() {
+    alert('My Name is Jake. I am collie mix. $123');
+}
+console.log();
+
+//to display dogprofile 3
+
+
+function myProfileFour() {
+    alert('My Name is Duffy. I am brown lab. $123');
+}
+console.log();
+
+//to display dogprofile 4
