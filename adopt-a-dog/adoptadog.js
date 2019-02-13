7fe134e9cd99fb2da186b909a9f8ca971dcb18a2
calinton <charlottelinton@users.noreply.github.com>
2019-02-12 19:40:49 -0800

    test code added

--- adopt-a-dog/adoptadog.js
+++ adopt-a-dog/adoptadog.js
@@ -5,3 +5,18 @@ alert('Name: ' + name + ', ' + 'Breed: ' + breed + ', ' + 'Cost: ' + '$' + cost)
 function dogCost(cost) {
   alert('The total is: ' + '$' + cost);
 }
+
+
+
+// Attempt at running total. Will log accumulating total when given
+// values in js, but does not work when I call the function and assign value in html
+// let total = 0;
+//
+//   function adoptionFee(cost) {
+//     total = total + cost;
+//     return total;
+//   }
+//
+//   adoptionFee(5);
+//   adoptionFee(5);
+//   console.log(total);
