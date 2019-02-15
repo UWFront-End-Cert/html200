8140d7811f897179e53ffa5f6e0507fc7d1a8e9e
catptrsn <43868078+catptrsn@users.noreply.github.com>
2019-02-14 20:03:52 -0800

    cleaned up code

--- adopt-a-dog/checkout.js
+++ adopt-a-dog/checkout.js
@@ -1,8 +1,19 @@
-function getSum() {
-  let p = 123.45;
-    if (p + 0) {
-      alert(p);
-    } else if (p + p) {
-      alert (p + p)
-    }
+function adoptThisDog() {
+  let adoptionFees = 123.45;
+  let total = 0;
+  total += adoptionFees;
+  console.log(total);
 }
+
+const dog1 = 'Name: Murphy, cost $123.45, breed: unknown';
+const dog2 = 'Name: Poppy, cost $123.45, breed: Shih tzu mix';
+const dog3 = 'Name: Jack, cost $123.45, breed: beagle';
+const dog4 = 'Name: Duffy, cost $123.45, breed: pitbull mix';
+const dog5 = 'Name: Lucas, cost $123.45, breed: lab mix';
+const dog6 = 'Name: Jake, cost $123.45, breed: labrador retriever';
+const dog7 = 'Name: Angus, cost $123.45, breed: golden retriever';
+const dog8 = 'Name: Violet, cost $123.45, breed: chihuahua';
+
+// function showDogInfo(dog) {
+//   alert(dog);
+// }
