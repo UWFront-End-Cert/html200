d9c8a65d1a7b99d9e06f5512a85fd70058299af2
An Dam <andam85@gmail.com>
2019-02-14 18:45:39 -0800

    fixed hw4 on have onclick

--- hw4/script1.js
+++ hw4/script1.js
@@ -1,7 +1,13 @@
-let num1 = prompt("Choose a number");
-let num2 = prompt("Choose your 2nd number")
-function numMulti(number1,number2){
-  return num1 * num2;
+let num1 = 0;
+let num2 = 0;
+
+function getNum(){
+  num1 = prompt("Choose a number");
+  num2 = prompt("Choose your 2nd number");
 }
 
-alert('the product of your numbers is: ' + numMulti(num1,num2));
+
+function numMulti(number1,number2){
+  let product = num1 * num2;
+  alert(product);
+}
---
+++ lesson5/javascript1.js
@@ -0,0 +1,8 @@
+let num = prompt('enter a number');
+
+function squareNum(numberToSquare){
+  let squaredNumber = numberToSquare * numberToSquare;
+  alert(squaredNumber)
+}
+
+squareNum(number(num);
