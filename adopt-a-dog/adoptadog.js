b5612c0396f149a12ccde6b952ef4f39040f8d58
calinton <charlottelinton@users.noreply.github.com>
2019-02-15 16:57:14 -0800

    running total error fixed

--- adopt-a-dog/adoptadog.js
+++ adopt-a-dog/adoptadog.js
@@ -2,21 +2,13 @@ function dogInfo(name, breed, cost) {
 alert('Name: ' + name + ', ' + 'Breed: ' + breed + ', ' + 'Cost: ' + '$' + cost);
 }
 
-function dogCost(cost) {
-  alert('The total is: ' + '$' + cost);
-}
+// function dogCost(cost) {
+//   alert('The total is: ' + '$' + cost);
+// }
 
+let total = 0;
 
-
-// Attempt at running total. Will log accumulating total when given
-// values in js, but does not work when I call the function and assign value in html
-// let total = 0;
-//
-//   function adoptionFee(cost) {
-//     total = total + cost;
-//     return total;
-//   }
-//
-//   adoptionFee(5);
-//   adoptionFee(5);
-//   console.log(total);
+  function adoptionFee(cost) {
+    total = total + cost;
+    alert('The total is: ' + '$' + total);
+  }
