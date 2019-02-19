29aefb4ef251523551721b22103bd04c00048417
Meghan Mullally <ms.mullally27@gmail.com>
2019-02-19 01:03:59 -0800

    function and prompt

---
+++ lesson5-1/lesson5.js
@@ -0,0 +1,13 @@
+//Online Banking 
+
+function OnlineBank() {
+  let bank = prompt('Q to quit, W to withdraw, D to deposit, B to view balance');
+
+  let balance = 1200;
+  let deposit = 0;
+  let withdraw  = 0;
+
+
+
+
+}
