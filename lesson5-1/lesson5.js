03834be06d18b719726e33d75bce75ec46ca8aa9
Meghan Mullally <ms.mullally27@gmail.com>
2019-02-19 02:12:38 -0800

    fixed the quotations and spacing

--- lesson5-1/lesson5.js
+++ lesson5-1/lesson5.js
@@ -1,34 +1,34 @@
-//Online Banking 
+// Online Banking 
 // list of 4 actions - Q , W , B , D
 // program loop 
 // While Switch Case: Case 1 Case 2 ....
 
 
 function OnlineBank() {
-  let bank = prompt ('Q to quit, W to withdraw, D to deposit, B to view balance');
+  let bank = prompt("Q to quit, W to withdraw, D to deposit, B to view balance");
 
   let balance = 1200;
   let deposit = 0;
   let withdraw = 0;
 
-  while (banking === 'Q' && banking === 'q') {
-    switch (banking)  {
-      case 'b':
-      case 'B':
+  while (bank === "Q" && bank === "q") {
+    switch (bank)  {
+      case "b":
+      case "B":
       // b & B for Balance 
-        alert('Hello! Your balance is:' + balance);
+        alert("Hello! Your balance is:" + balance);
           bank();
            break;
-        case 'W':
-        case 'w':
+        case "W":
+        case "w":
         // w and W for withdrawl
-         alert('Withdrawal Amount is:' + withdraw);
+         alert("Withdrawal Amount is:" + withdraw);
          bank();
           break;
-            case 'd':
-            case 'D':
+            case "d":
+            case "D":
             // d and D for deposit 
-              alert('Deposit Amount is:' + deposit);
+              alert("Deposit Amount is:" + deposit);
               bank();
                 break;
                 default:
