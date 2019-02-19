f0021c62321dfa5bde0180623e0e720a7b3b5e45
tom <tommy.surjadi@gmail.com>
2019-02-19 07:26:19 -0800

    Amended cart logo section

--- dogAlert.js
+++ dogAlert.js
@@ -10,42 +10,9 @@ function myProfile(name, breed, costs) {
 //to display greetings. Replaced by onMouseOver (header adop a dog) instead.
 
 
-// function myProfileTwo() {
-//     alert('My Name is Poppy. I am husky. $123');
-// }
-// console.log();
-
-//to display dogprofile 1
-
-
-// //to display dogprofile 2
-
-
-// function myProfileThree() {
-//     alert('My Name is Jake. I am collie mix. $123');
-// }
-// console.log();
-
-// //to display dogprofile 3
-
-
-// function myProfileFour() {
-//     alert('My Name is Duffy. I am brown lab. $123');
-// }
-// console.log();
-
-// //to display dogprofile 4
-
-// const dog1 = 'name = murphy, cost = 123, breed= beagle';
-// const dog2 = 'name = blake, cost = 123, breed = collie';
-// const dog3 = 
-
-// then after alert after picture ('alert dog1')
-
 let totalAdoptionFees =0;
 
 function adoptThisDog(adoptionFees) {
     totalAdoptionFees += adoptionFees;
     console.log(totalAdoptionFees); 
 }
-
