e2045a0c15c6132ed76bbbd544de7311cbfc0c96
Vu Nguyen <vunguyen@D-10-19-189-8.dhcp4.washington.edu>
2019-02-21 18:10:53 -0800

    Fixed negative bug and added low money feature

--- lesson05/bank.js
+++ lesson05/bank.js
@@ -2,35 +2,42 @@ let balance = 1000;
 
 while (balance > 0) {
 
-let enteredOption = prompt('Enter Q to quit, Enter W to withdraw, Enter D to deposit, Enter in B to view balance.')
+  let enteredOption = prompt('Enter Q to quit, Enter W to withdraw, Enter D to deposit, Enter in B to view balance.')
 
-if (enteredOption.toUpperCase() == 'Q') { break; }
-
-  else if (enteredOption.toUpperCase() == 'W') {
+  if (enteredOption.toUpperCase() == 'Q') {
+    break;
+  } else if (enteredOption.toUpperCase() == 'W') {
     let withdrawAmount = prompt('Enter in amount to withdraw: ');
     if (withdrawAmount > balance) {
       alert('You cannot withdraw more than your balance');
-    }
-    else if (withdrawAmount <= 0) {
+    } else if (withdrawAmount <= 0) {
       alert('You cannot withdraw negative or 0.')
-    }
-    balance = balance - Number(withdrawAmount);
-    alert('New balance is: $' + balance);
+    } else {
+      balance = balance - Number(withdrawAmount);
+      if (balance <= 300) {
+        alert('Your balance is low');
+      }
     }
 
-    else if (enteredOption.toUpperCase() == 'D') {
+    alert('New balance is: $' + balance);
+  } else if (enteredOption.toUpperCase() == 'D') {
     let depositAmount = prompt('Enter in amount to deposit: ');
+
     if (depositAmount > 50000) {
-      alert('You cannot withdraw more than $50,000');
-    }
-    balance = balance + Number(depositAmount);
-    alert('New balance is: $' + balance);
+      alert('You cannot deposit more than $50,000');
+    } else {
+      balance = balance + Number(depositAmount);
+
     }
 
-    else if (enteredOption.toUpperCase() == 'B') {
     alert('New balance is: $' + balance);
+
+  } else if (enteredOption.toUpperCase() == 'B') {
+
+    alert('New balance is: $' + balance);
+
   }
 
- }
+}
 
- alert("Thanks for visiting our bank! See you again.")
+alert("Thanks for visiting our bank! See you again.")
