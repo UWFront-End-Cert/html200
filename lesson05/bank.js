60f3c21d934b3fa4aa78c4d08439b7c56373ed5e
catptrsn <43868078+catptrsn@users.noreply.github.com>
2019-02-21 18:50:49 -0800

    adjusted javascript

--- lesson05/bank.js
+++ lesson05/bank.js
@@ -1,6 +1,6 @@
 function bankingOperations() {
   let quit = false;
-  let startingBalance = 1000;
+  let balance = 1000;
 
   while (quit === false) {
     let input = prompt('Enter Q to quit, W to make a withdrawal, D to deposit, and B to check your balance.');
@@ -9,7 +9,7 @@ function bankingOperations() {
       quit = true;
     } else if (input === 'W' || input === 'w') {
       let withdrawal = prompt('How much would you like to withdraw?');
-      let result = startingBalance - withdrawal;
+      let result = balance - withdrawal;
           if (result <= 100) {
             alert('Warning: This withdrawal will leave you with a low balance!');
           } else if(result <= 0) {
@@ -22,12 +22,14 @@ function bankingOperations() {
           }
       console.log(result);
     } else if (input === 'D' || input === 'd') {
-      let deposit = prompt('How much would you like to deposit?');
-      if (deposit >= 50000) {
+        let depositAmount = prompt('How much would you like to deposit?');
+        if (depositAmount >= 50000) {
         alert('Deposits must be less than $50,000.');
-      }
+        balance += Number(depositAmount);
+        }
+        console.log(balance);
     } else if (input === 'B' || input === 'b') {
-      alert(startingBalance);
+      alert(balance);
     }
   }
 }
