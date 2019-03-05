999cfce0e570624f88fc528d78202d3466e65d66
setupshop <gedion.sami@gmail.com>
2019-03-04 18:14:09 -0800

    addind media queries

--- adopt-a-dog/js/adopt-a-dog.js
+++ adopt-a-dog/js/adopt-a-dog.js
@@ -1,7 +1,12 @@
-const button = document.querySelector('button');
-button.addEventListner(Click, adoptTally);
+// const button = document.querySelector('button');
+// button.addEventListner(Click, adoptTally);
 let totalTally = 0;
-function adoptTally {
+function adoptTally() {
   totalTally += 123;
-  alert('totalTally');
+  alert(totalTally);
+}
+function dogInfo() {
+  let img = document.getElementsByClassName('dog-card')
+  let alt = img.getAttribute("alt");
+  alert(alt);
 }
