2c08170baf70a8681fc6f0d8eed364d30259ecb7
Meghan Mullally <ms.mullally27@gmail.com>
2019-03-06 18:05:45 -0800

    added dog1, dog2 ....

--- adopt-a-dog/adopt-a-dog.js
+++ adopt-a-dog/adopt-a-dog.js
@@ -6,9 +6,9 @@
 
 //Dog's Information - Name, Breed and Price
 
-let dogName;
-let dogBreed;
-let dogDescr;
+// let dogName;
+// let dogBreed;
+// let dogDescr;
 let dogPrice = 123.45;
 let checkoutTotal = 0;
 
@@ -33,3 +33,17 @@ function dogInfo(click.img) {
 // my side note/idea: later on change the totals of each dog so they are all different $$ 
 
 //'Name: ' + dogName + ' is ' + dogDescr + ' and ' + dogBreed + ' $ ' + dogPrice
+
+
+const dog1 = 'Name: Murphy, Cost: $123.45, Breed: Lab';
+const dog2 = 'Name: Poppy, Cost: $123.45, Breed: Havanese';
+const dog3 = 'Name: Jack, Cost: $123.45, Breed: Lab';
+const dog4 = 'Name: Duffy, Cost: $123.45, Breed: lab';
+const dog5 = 'Name: Lucas, Cost: $123.45, Breed: Unknown';
+const dog6 = 'Name: Jake, Cost: $123.45, Breed: Golden Retriever';
+const dog7 = 'Name: Angus, Cost: $123.45, Bred: Lab';
+const dog8 = 'Name: Violet, Cost: $123.45, Breed: Chihuahua';
+const dog9 = 'Name: Piper, Cost: $123.45, Breed: Terrier';
+const dog10 = 'Name: Maximus, Cost: $123.45, Breed: Lab';
+const dog11 = 'Name: Luna, Cost: $123.45, Breed: Pitbull';
+const dog12 = 'Name: Stella, Cost: $123.45 Breed: Beagle?';
