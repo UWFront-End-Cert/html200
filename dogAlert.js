6b0c5b6f38ad8f83dd1d0552071c72d22860770f
tom <tommy.surjadi@gmail.com>
2019-03-17 11:00:19 -0700

    Removed html content of blog page and inject content and picture via js, created contact page -from the intial checkout page and delete checkout page

--- dogAlert.js
+++ dogAlert.js
@@ -21,3 +21,27 @@ function showDogInfo(dog) {
     console.log(dog);
     alert(dog);
 }
+
+// ======ADD blog-para======================================================
+
+// const x = blog-para;
+
+// for(let x = 0; x <blog-para.length; x++) {
+//     console.log(blog-para);
+// }
+
+document.getElementsByClassName('blog-para')[0].innerHTML = 'Blog1 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae illum minus, praesentium tempora expedita voluptatum qui cumque pariatur tempore dolores quos voluptatibus, ut mollitia delectus molestias. Enim incidunt cumque quaerat. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae illum minus, praesentium tempora expedita voluptatum qui cumque pariatur tempore dolores quos voluptatibus, ut mollitia delectus molestias. Enim incidunt cumque quaerat.';
+document.getElementsByClassName('blog-para')[1].innerHTML = 'Blog2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae illum minus, praesentium tempora expedita voluptatum qui cumque pariatur tempore dolores quos voluptatibus, ut mollitia delectus molestias. Enim incidunt cumque quaerat. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae illum minus, praesentium tempora expedita voluptatum qui cumque pariatur tempore dolores quos voluptatibus, ut mollitia delectus molestias. Enim incidunt cumque quaerat.';
+document.getElementsByClassName('blog-para')[2].innerHTML = 'Blog3 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae illum minus, praesentium tempora expedita voluptatum qui cumque pariatur tempore dolores quos voluptatibus, ut mollitia delectus molestias. Enim incidunt cumque quaerat. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae illum minus, praesentium tempora expedita voluptatum qui cumque pariatur tempore dolores quos voluptatibus, ut mollitia delectus molestias. Enim incidunt cumque quaerat.';
+
+// ======ADD blog-header======================================================
+
+document.getElementsByClassName('blog-header')[0].innerHTML = 'Travelling with your dogs';
+document.getElementsByClassName('blog-header')[1].innerHTML = 'How to Walk Multiple Dogs';
+document.getElementsByClassName('blog-header')[2].innerHTML = 'Teach Your Dogs to Fetch!';
+
+// ======ADD blog-picture======================================================
+
+document.getElementsByClassName('blog-picture')[0].src = 'images/patrick-hendry-221863-unsplash.jpg';
+document.getElementsByClassName('blog-picture')[1].src = 'images/matt-nelson-259365-unsplash.jpg';
+document.getElementsByClassName('blog-picture')[2].src = 'images/wyatt-ryan-367017-unsplash.jpg';
