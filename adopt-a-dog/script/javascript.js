8ac8b48f2b7195937c2d0d768d90d7fcb3b8daf9
sanjali83 <anjali.raghuwanshi@gmail.com>
2019-03-18 23:20:36 -0700

    Assignment 9 Uploaded

--- adopt-a-dog/script/javascript.js
+++ adopt-a-dog/script/javascript.js
@@ -1,26 +1,106 @@
 let dogInfo =[
-  {name : "Murphy" , breed : "Great Dane" , age : 2 , cost : 123.45},
-  {name : "Poppy", breed : "Samoyed" , age : 1 , cost : 123.45},
-  {name : "Jack" , breed : "Pug" , age: 3 , cost : 123.45},
-  {name : "Duffy", breed : "Husky" , age: 2.5 , cost : 123.45},
-  {name : "Lucas", breed : "Labrador" , age : 6, cost : 123.45},
-  {name : "Jake", breed : "Tosa" , age : 5, cost : 123.45},
-  {name : "Angus", breed: "Beagle" , age : 1.2, cost : 123.45},
-  {name : "Violet", breed: "Poodle", age : 4, cost : 123.45},
-  {name : "Piper", breed: "Doberman" , age : 3, cost : 123.45},
-  {name : "Maximus", breed: "Rootweiler" , age : 5, cost : 123.45},
-  {name : "Luna", breed: "Dalmatian" , age : 3, cost : 123.45},
-  {name : "Stella", breed: "Bullterrier" , age : 2, cost : 123.45},
+  {name : "Murphy" , breed : "Great Dane" , age : 2 , cost : 123.45 , img : "images/alex-lacayo-756508-unsplash.jpg", altext : "Murphy Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name : "Poppy", breed : "Samoyed" , age : 1 , cost : 123.45, img : "images/nikolay-tchaouchev-785309-unsplash.jpg", altext : "Poppy Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name : "Jack" , breed : "Pug" , age: 3 , cost : 123.45, img : "images/marcus-wallis-471438-unsplash.jpg", altext : "Jack Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."  },
+  {name : "Duffy", breed : "Husky" , age: 2.5 , cost : 123.45, img : "images/duffy-brook-350225-unsplash.jpg", altext : "Duffy Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."  },
+  {name : "Lucas", breed : "Labrador" , age : 6, cost : 123.45, img : "images/tucker-good-589776-unsplash.jpg", altext : "Lucas Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Jake", breed : "Tosa" , age : 5, cost : 123.45, img : "images/mitchell-orr-237216-unsplash.jpg", altext : "Jake Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Angus", breed: "Beagle" , age : 1.2, cost : 123.45, img : "images/jonathan-daniels-416786-unsplash.jpg", altext : "Angus Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Violet", breed: "Poodle", age : 4, cost : 123.45, img : "images/kenan-kitchen-1136827-unsplash.jpg", altext : "Violet Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Piper", breed: "Doberman" , age : 3, cost : 123.45, img : "images/jamie-street-562280-unsplash.jpg", altext : "Piper Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name : "Maximus", breed: "Rootweiler" , age : 5, cost : 123.45, img : "images/jisu-lee-55400-unsplash.jpg", altext : "Maximus Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name : "Luna", breed: "Dalmatian" , age : 3, cost : 123.45,  img : "images/jonathan-daniels-416786-unsplash.jpg", altext : "Luna Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+  {name : "Stella", breed: "Bullterrier" , age : 2, cost : 123.45, img : "images/alicia-gauthier-253619-unsplash.jpg", altext : "Stella Dog Picture", info : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
 ]
+
+var blogDetails = [
+{img : "images/patrick-hendry-221863-unsplash.jpg" , imgtext : "Girl setting with her Dog on Cliff" , heading : "Traveling With Your Dog" , info1 : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." , info2 : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+{img : "images/matt-nelson-259365-unsplash.jpg" , imgtext : "Many Dogs With Leash" , heading : "How To Walk Multiple Dogs" , info1 : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." , info2 : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+{img : "images/wyatt-ryan-367017-unsplash.jpg" , imgtext : "Girl Training her Dog" , heading : "Teach Your Dog To Fetch" , info1 : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." , info2 : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+];
+
+function dogtilesload() {
+    var container = document.getElementById('wrapperDogs');
+
+    webpageOnLoad();
+
+    for (i = 0 ; i < dogInfo.length ; i++){
+       const box = document.createElement('article');
+       box.setAttribute ('class','dog-info');
+
+       box.innerHTML = '<img src = ' + dogInfo[i].img + ' alt = ' + dogInfo[i].altext + ' class = "dog-img" onclick = "displayInfo(0)" >  <h3> ' + dogInfo[i].name + '</h3> <h5><b> Cost to Adopt : </b> ' + dogInfo[i].cost + '</h5> <p>' + dogInfo[i].info + '</p> <button type="button" class="checkout-button" onclick = "checkoutCalculation(0)" > Adopt </button>';
+
+       container.appendChild(box);
+    }
+}
+
+function indexdogtilesload() {
+    const container = document.getElementById('wrapperMain');
+    const button = document.getElementById('buttonArticle')
+    webpageOnLoad();
+
+    for (i = 0 ; i < 6 ; i++){
+       const box = document.createElement('article');
+       box.setAttribute ('class','dog-info');
+
+       box.innerHTML = '<img src = ' + dogInfo[i].img + ' alt = ' + dogInfo[i].altext + ' class = "dog-img" onclick = "displayInfo(0)" >  <h3> ' + dogInfo[i].name + '</h3> <h5><b> Cost to Adopt : </b> ' + dogInfo[i].cost + '</h5> <p>' + dogInfo[i].info + '</p> <button type="button" class="checkout-button" onclick = "checkoutCalculation(0)" > Adopt </button>';
+
+       container.appendChild(box);
+
+       container.insertBefore(box,button);
+    }
+
+
+}
+
+function blogTilesLoad() {
+    const page = document.getElementById('blogPage');
+
+    webpageOnLoad();
+
+    for (let i = 0; i < blogDetails.length; i++) {
+      const box = document.createElement('article');
+      box.setAttribute('class','blog-topic');
+
+      page.appendChild(box);
+
+      const image = document.createElement('img');
+      image.src = blogDetails[i].img ;
+      image.setAttribute ('alt', blogDetails[i].imgtext);
+      image.setAttribute ('class', 'blog-img');
+
+      box.appendChild(image);
+
+      const textbox = document.createElement('div');
+      textbox.setAttribute('class','blog-content');
+
+      box.appendChild(textbox);
+
+      const title = document.createElement('h3');
+      title.textContent = blogDetails[i].heading;
+
+      textbox.appendChild(title);
+
+      const details1 = document.createElement('p');
+      details1.textContent = blogDetails[i].info1;
+
+      textbox.appendChild(details1);
+
+      const details2 = document.createElement('p');
+      details2.textContent = blogDetails[i].info2;
+
+      textbox.appendChild(details2);
+    }
+}
 
 function displayInfo(i) {
     alert ('Meet '+ dogInfo[i].name + ', a ' + dogInfo[i].age + '-year-old ' + dogInfo[i].breed + ' !' + 'The adoption price is : $' + dogInfo[i].cost);
 }
 
-function checkoutCalculation(price) {
+function checkoutCalculation(i) {
   let temp = sessionStorage.getItem("cartAmount");
 
-  sessionStorage.setItem("cartAmount", (Number(temp) + Number(price)));
+  sessionStorage.setItem("cartAmount", (Number(temp) + Number(dogInfo.cost[i])));
 
   document.getElementById('cartAmount').innerHTML = '$' + sessionStorage.getItem("cartAmount");
 
@@ -45,4 +125,78 @@ function formOnLoad() {
 
 function thankYou() {
   alert ('Thank You for Your Support ....!');
+}
+
+
+function ContactUsForm() {
+    const variables = ['FirstName', 'LastName', 'EmailId','Comments'];
+    const form = document.getElementById('contactUs-form');
+
+    webpageOnLoad();
+
+    for (let i = 0; i < variables.length; i++) {
+        const label = document.createElement('label');
+        label.textContent = variables[i];
+        label.setAttribute ('for', variables[i]);
+
+        form.appendChild(label);
+
+        if (variables[i] === 'Comments') {
+            var textarea = document.createElement("TEXTAREA");
+            textarea.setAttribute('id', variables[i]);
+            form.appendChild(textarea);
+        }  else {
+            var input = document.createElement("input");
+            input.setAttribute('id', variables[i]);
+            //if (variables[i] === 'EmailId') {
+            //    input.setAttribute ('type', 'email');
+            //}
+            form.appendChild(input);
+        }
+    }
+
+    const submitButton= document.createElement('button');
+    submitButton.setAttribute('type','submit');
+    submitButton.setAttribute('class', 'checkout-button');
+    submitButton.textContent = 'Submit';
+
+    form.appendChild(submitButton);
+
+    try {
+        form.addEventListener('submit', function() {
+            var error = 0;
+            for (i = 0; i < variables.length ; i++) {
+              const string = document.getElementById(variables[i]).value;
+              console.log(variables[i] + " values is = " + string);
+              if( string.trim() === "") {
+                 alert ('Kindly enter all data in the form.');
+                 error = 1;
+                 document.getElementById(variables[i]).focus();
+                 break;
+              }
+              else if (variables[i] === "EmailId" &&  invalidEmail(string)) {
+                  alert ('Kindly enter correct email address...!');
+                  error = 1;
+                  document.getElementById(variables[i]).focus();
+                  break;
+              }
+            }
+            if (error === 0) {
+              alert ("Your submission has been received");
+            }
+      });
+
+      function invalidEmail(word) {
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(word.match(mailformat)) {
+          return false;
+        }
+        else {
+          return true;
+        }
+      }
+  }
+  catch (err) {
+    console.log("Error Handled");
+  }
 }
