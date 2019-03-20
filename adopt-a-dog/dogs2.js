091a438e5da3b670c8212e1095a1f1ccc61ee133
Duy-An Q Dam <duy-an.q.dam@boeing.com>
2019-03-20 12:55:20 -0700

    made dogs2.html and dog2.js to allow dynamic creation of dog ties

---
+++ adopt-a-dog/dogs2.js
@@ -0,0 +1,41 @@
+let dogs = [
+{name:'Murphy', breed: 'Lab', price: 123.45, pic:'images/alex-lacayo-756508-unsplash.jpg'}
+,{name:'Poppy', breed: 'Maltese', price: 123.45, pic:'images/nikolay-tchaouchev-785309-unsplash.jpg'}
+,{name:'Jack', breed: 'Jack Russell', price: 123.45, pic:'images/marcus-wallis-471438-unsplash.jpg'}
+,{name:'Duffy', breed: 'Pitbull', price: 123.45, pic:'images/duffy-brook-350225-unsplash.jpg'}
+,{name:'Lucas', breed: 'Lab', price: 123.45, pic:'images/tucker-good-589776-unsplash.jpg'}
+,{name:'Jake', breed: 'Lab', price: 123.45, pic:'images/mitchell-orr-237216-unsplash.jpg'}
+];
+
+
+tileContainer = document.getElementsByClassName('tile-container')[0]
+
+for ( let i=0; i < dogs.length; i++){
+  const dogTile = document.createElement('div');
+  dogTile.className= 'dog-tile';
+
+  const tileImage= document.createElement('img');
+  tileImage.className= 'small-dog-picture'
+  tileImage.setAttribute('src',dogs[i].pic)
+
+  const dogTileBody = document.createElement('div');
+  dogTileBody.className= 'tile-body';
+
+  const dogHeader = document.createElement('h3');
+  dogHeader.textContent = dogs[i].name;
+
+  const dogText = document.createElement('p');
+  dogText.innerHTML = '<p><strong>Cost to adopt:</strong>$'+dogs[i].price+'<br>'+'Corrum volorit iandae nimaxim cum restia volor reicid ut et etur sunt arum rendae pla endis re ea erum, qui doluptae'+'</p>';
+
+  const dogButton = document.createElement('button');
+  dogButton.setAttribute('onclick',"runningTotal()");
+  dogButton.className = 'adopt';
+  dogButton.textContent='Adopt'
+
+  tileContainer.appendChild(dogTile);
+  dogTile.appendChild(tileImage);
+  dogTile.appendChild(dogTileBody);
+  dogTileBody.appendChild(dogHeader);
+  dogTileBody.appendChild(dogText);
+  dogTileBody.appendChild(dogButton);
+}
--- adopt-a-dog/script1.js
+++ adopt-a-dog/script1.js
@@ -51,4 +51,17 @@ let total = 0;
 function runningTotal() {
    total = total + 123.45;
    alert('total cost ($): '+total);
-}
+};
+
+let dogs = [
+dog1 ={name:'Murphy', breed: 'Lab', price: 123.45, pic:'images/alex-lacayo-756508-unsplash.jpg'}
+,dog2={name:'Poppy', breed: 'Maltese', price: 123.45, pic:'images/nikolay-tchaouchev-785309-unsplash.jpg'},
+,dog3={name:'Jack', breed: 'Jack Russell', price: 123.45, pic:'images/marcus-wallis-471438-unsplash.jpg'}
+,dog4={name:'Duffy', breed: 'Pitbull', price: 123.45, pic:'images/duffy-brook-350225-unsplash.jpg'}
+,dog5={name:'Lucas', breed: 'Lab', price: 123.45, pic:'images/tucker-good-589776-unsplash.jpg'}
+,dog6={name:'Jake', breed: 'Lab', price: 123.45, pic:'images/mitchell-orr-237216-unsplash.jpg'}
+];
+
+
+console.log(dogs[0].name)
+console.log(dogs.length)
