42cb628d0718a557cd44cdd55b690298de121f81
setupshop <gedion.sami@gmail.com>
2019-03-20 11:08:18 -0700

    adding more JS for contact form

--- adopt-a-dog/js/adopt-a-dog.js
+++ adopt-a-dog/js/adopt-a-dog.js
@@ -10,3 +10,53 @@ function dogInfo() {
   let alt = img.getAttribute("alt");
   alert(alt);
 }
+function submitSuccess() {
+  alert('Information Successfully submitted');
+}
+
+let formElement = document.querySelector('fieldset');
+console.log(formElement);
+
+formElement.addEventListner('submit', function(e){
+  e.preventDefault();
+  const firstName =formElement.elements[0].value;
+  const lastNamename = formElement.elements[1].value;
+
+  const dataToSubmit = {
+    name: inputValue,
+    dog: dogname
+  };
+  console.log(dataToSubmit);
+})
+
+
+const form = document.createElement('form');
+const feilds = ['firstName', 'lastName'];
+
+for (let i = 0; i < feilds.length; i++) {
+  const label = document.createElement('label');
+  label.textContent = feilds[i];
+  label.setAttibute('for', feilds[i]);
+
+  const input = document.createElement('input');
+  input.setAttibute('id', feilds[i]);
+
+  form.appendChild(label);
+  form.appendChild(input);
+
+}
+const submitButton = document.createElement('button');
+submitButton.setAttibute('type', 'submit');
+submitButton.textContent = 'Submit';
+
+let fromContainer = document.querySelector('.form-container');
+
+form.addEventListner('submit', function(e){
+  // do stuff
+  e.preventDefault();
+  console.log('form submitted');
+  console.log(form.elements[0].value);
+  console.log(form.elements[1].value);
+})
+
+formContainer.appendChild(form);
