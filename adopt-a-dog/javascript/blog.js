ceea1dfca20c3a78d49323b00c1e916d58d88f94
Mikyas Ayele <mayele23@Mikyass-MBP.PK5001Z>
2019-03-25 16:48:50 -0700

    linked jquery correctly and added effects to dog tiles

--- adopt-a-dog/javascript/blog.js
+++ adopt-a-dog/javascript/blog.js
@@ -1,11 +1,38 @@
-let subHeading = document.querySelectorAll('h3');
-let images = document.querySelectorAll('img');
-// let para = document.querySelectorAll('p');
+// let subHeading = document.querySelectorAll('h3');
+// let images = document.querySelectorAll('img');
+// // let para = document.querySelectorAll('p');
+//
+// subHeading[0].innerHTML = '<h3>Traveling With Your Dog</h3>';
+// subHeading[1].innerHTML = '<h3>Traveling With Your Dog</h3>';
+// subHeading[2].innerHTML = '<h3>Traveling With Your Dog</h3>';
+//
+// images[2].src = 'images/patrick-blog.jpg';
+// images[3].src = 'images/joao-blog.jpg';
+// images[4].src = 'images/wyatt-blog.jpg';
 
-subHeading[0].innerHTML = '<h3>Traveling With Your Dog</h3>';
-subHeading[1].innerHTML = '<h3>Traveling With Your Dog</h3>';
-subHeading[2].innerHTML = '<h3>Traveling With Your Dog</h3>';
+// let subHeading = document.querySelectorAll('h3');
+//
+//
+// for (let i = 0; i < subHeading.length; i++) {
+//  subHeading[0].innerHTML = '<h3>Traveling With Your Dog</h3>';
+// }
 
-images[2].src = 'images/patrick-blog.jpg';
-images[3].src = 'images/joao-blog.jpg';
-images[4].src = 'images/wyatt-blog.jpg';
+
+// const blogPosts = [
+//   {
+//     image: 'images/patrick-blog.jpg',
+//     alt: 'girl sitting with a dog',
+//     headLine: 'Traveling with a dog',
+//     blogText: 'Blog is a blog of things'
+//   }
+// ]
+//
+// const blogPostContainer = document.querySelector('.blog-container');
+//
+//
+// for(let i = 0; i < blogPosts.length; i++) {
+//   let blogPost = document.createElement('article');
+//   blogPost.innerHTML = '<h1>' + blogPosts[i].headLine + '</h1>' + '<p>' + blogPosts[i].blogText + '</p>'
+//   blogPostContainer.appendChild(blogPost);
+//
+// }
---
+++ adopt-a-dog/javascript/jquery.js
@@ -0,0 +1,21 @@
+$('#murphy').mouseover(function() {
+  this.src = './images/murphy.jpg';
+});
+$('#murphy').mouseout(function() {
+  this.src = './images/murphyblack.jpeg';
+});
+
+$('#poppy').mouseover(function() {
+  this.src = './images/poppy.jpg';
+});
+$('#poppy').mouseout(function() {
+  this.src = './images/poppyblack.jpeg';
+});
+
+$('#jack').mouseover(function() {
+  this.src = './images/jack.jpg';
+});
+
+$('#jack').mouseout(function() {
+  this.src = './images/jackblack.jpeg';
+});
