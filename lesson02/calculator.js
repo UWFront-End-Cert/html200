eeb5fc19684cdf8939bf2eec33b3a86a908d412d
Mbochman034 <michellebochman@comcast.net>
2020-12-10 14:19:53 -0500

    File update

---
+++ lesson02/calculator.js
@@ -0,0 +1,11 @@
+var x=4;
+var y=7;
+var z=8;
+var a=3;
+
+console.log (x + a / y);
+console.log (x + y);
+console.log (z - x % y);
+console.log (x * (2 + y) / a);
+console.log (x * ( z / x));
+console.log (z % x);
