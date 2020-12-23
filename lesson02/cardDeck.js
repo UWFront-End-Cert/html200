27f9110b258987dd794bef6c2aaeaa3f9aa9e212
djr206 <myEmail@wherever.com>
2020-12-22 22:05:10 -0800

    L2C5 the most fun

---
+++ lesson02/cardDeck.js
@@ -0,0 +1,87 @@
+// L2C5 CardDeck
+
+// Write a program called cardDeck.js. Create the code in codpen.io and 
+// then copy that code into a document and add to your GitHub in the 
+// Lesson02 directory.
+
+// Write a program that prints out the values of a deck of cards.
+
+// Save your work to the Lesson02 directory, on your lesson2-challenge5 
+// Git branch.
+
+
+let card_values, card_suits, text, csLen, cvLen, i, j;
+card_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+card_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+csLen = card_suits.length;
+cvLen = card_values.length;
+for (i = 0; i < cvLen; i++) {
+    console.log(card_values[i]);
+    for (j = 0; j < csLen; j++) {
+      console.log(' of ' + card_suits[j]);
+  }
+}
+
+
+// const card_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+// console.log(card_suits[0] + card_values);
+// console.log(card_values + card_suits[0]);
+// for (let x = 10; x >= 0; x--) {
+//     console.log(x);
+// }
+// for (card_suits[0], card_suits<[4], card_suits++) {
+//   console.log(card_values + ' of ' card_suits);
+// }
+  // console.log(card_values + ' of ' card_suits);
+
+// var fruits, text, fLen, i;
+// fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fLen = fruits.length;
+
+// text = "<ul>";
+// for (i = 0; i < fLen; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+// text += "</ul>";
+// let card_values, card_suits, text, csLen, cvLen, i;
+// card_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+// card_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+// csLen = card_suits.length;
+// cvLen = card_values.length;
+// // text = (card_values + ' of ' card_suits);
+// for (i = 0; i < csLen i++) {
+//     for (i = 0; i < cvLen; i++) {
+//       console.log(card_values + ' of ' + card_suits);
+    // console.log(text += card_values[i]);
+// }
+  
+// }
+
+
+// const card_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+// console.log(card_suits[0] + card_values);
+// console.log(card_values + card_suits[0]);
+// for (let x = 10; x >= 0; x--) {
+//     console.log(x);
+// }
+// for (card_suits[0], card_suits<[4], card_suits++) {
+//   console.log(card_values + ' of ' card_suits);
+// }
+  // console.log(card_values + ' of ' card_suits);
+
+
+// const card_values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+// const card_suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+
+// console.log(card_suits + card_values);
+
+// const ah = 'ace of hearts';
+// const ad = 'ace of diamonds';
+// const ac = 'ace of clubs';
+// const as = 'ace of spades';
+// const 1 = 'Ace';
+// for (hearts > 0 .and. hearts <= 10) {
+//     console.log
+// }
