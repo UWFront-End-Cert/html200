4aaa9dc957096dea92668175dbb16c7e852553d6
djr206 <myEmail@wherever.com>
2020-12-29 22:16:06 -0800

    l3c2 array exercise

---
+++ lesson03/l3-c2.js
@@ -0,0 +1,13 @@
+
+
+    // Make a string variable of a sentence with least five words and a period.
+    // Make an array from that sentence and console.log the third word.
+
+l3c2 = "Make a string variable of a sentence with at least five words and a period."
+    console.log(l3c2);
+    wordArray = l3c2.split(' ');
+    // wordArray.forEach(function(element) {console.log(element);});
+    console.log("The thrid word is " + "'" + wordArray[2] + "'" + ".");
+    // console.log(wordArray[2]);
+    console.log(wordArray);
+    
