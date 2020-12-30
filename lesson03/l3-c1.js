de02a895e12cdf63ffe01482217db803b3ff74d7
djr206 <myEmail@wherever.com>
2020-12-29 18:26:40 -0800

    dele unused directories/files

--- lesson03/l3-c1.js
+++ lesson03/l3-c1.js
@@ -7,17 +7,17 @@
 // let l3c1 = [];
 l3c1 = [["Do a console log after each item"], ["Create an array, with at least 5 strings"], ["Add an element to the end of the array"], ["Remove the third element"], ["Create a string from the elements and comma separate them"]];
 const origArr = l3c1;
-console.log(origArr);
-console.log(l3c1);
-console.log(l3c1[2]); // returns array value not text
+    console.log(origArr);
+    console.log(l3c1);
+    console.log(l3c1[2]); // returns array value not text
 let addedItem = l3c1.push("Remember, indexing is zero based.");
-console.log(addedItem); // why does this return a 6?
-console.log(l3c1[5] + " (previous element added to array)"); // here text
-console.log((l3c1[2]) + ' ' + (addedItem)); // text and a 6
+    console.log(addedItem); // why does this return a 6?
+    console.log(l3c1[5] + " (previous element added to array)"); // here text
+    console.log((l3c1[2]) + ' ' + (addedItem)); // text and a 6
 let splicedItem = l3c1.splice(2,1); // origArr loses element, why when const 
-console.log(l3c1);
+    console.log(l3c1);
 l3c1.join('. ') + ' The following element was removed from the array - ' + '(' + splicedItem + ').';
-// console.log(l3c1); // including line drops prev text output
+// console.log(l3c1); // including this line drops prev text output
 
 
 
