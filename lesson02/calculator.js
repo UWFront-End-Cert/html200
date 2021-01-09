28d77043ec406562a7b9e7c67195536ca82d420c
Dw3ntz <72217128+Dw3ntz@users.noreply.github.com>
2021-01-08 17:36:08 -0800

    track all files

---
+++ lesson02/calculator.js
@@ -0,0 +1,31 @@
+let wordOne = 'cat';
+let wordTwo = 'lady'; 
+
+console.log(wordOne + ' ' + wordTwo);
+// --> 'cat lady'
+
+
+const x = 'get a cat';
+const y = 'no more cats';
+
+
+if (20 / 5 === 2) {
+  console.log(x);
+} else {
+  console.log(y);
+}
+// --> 'no more cats'
+
+
+console.log(100 >= 200);
+// --> 'false'
+
+
+console.log("Kitteh" == "Kitteh")
+// --> 'true'
+
+
+console.log(- (11 * 2));
+// --> '-22'
+
+
