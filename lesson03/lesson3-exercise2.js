626d3333dca0e6b888a8e0e1e6d9a4a323fb3bc7
carolyngreeno <greeno.carolyn@gmail.com>
2021-08-06 15:23:26 -0700

    completed lesson 3, exercise 2

---
+++ lesson03/lesson3-exercise2.js
@@ -0,0 +1,7 @@
+// Make a string variable of a sentence with least five words and a period.
+let sentence = "I have lots of plants.";
+
+// Make an array from that sentence and console.log the third word.
+sentenceArray = sentence.split(" ");
+
+console.log(sentenceArray[2]);
