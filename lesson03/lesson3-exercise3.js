debc5a37283104c6e51a141514d35b73bdb54318
carolyngreeno <greeno.carolyn@gmail.com>
2021-08-09 18:05:46 -0700

    add comma in between index and item

--- lesson03/lesson3-exercise3.js
+++ lesson03/lesson3-exercise3.js
@@ -1,10 +1,11 @@
-// I couldn't figure out how to add the comma after the index and before the element returned by the array
-// :)
-
 // Create an array, with at least 5 strings
 let plants = ["monsteras", "calatheas", "pothos", "ferns", "peperomias"];
 
 // Use a for loop to console log all of the elements, plus the index and a comma
-plants.forEach(function (item, index, array) {
-  console.log(index, item);
+plants.forEach(function (index, item) {
+  console.log(item, ", ", index);
 });
+
+// Example:
+// 1,cats
+// 2,dogs
