0bc584eae49bd1011f69446827b82e9945c85384
carolyngreeno <greeno.carolyn@gmail.com>
2021-08-10 19:41:35 -0700

    changed file

--- lesson03/lesson3-exercise3.js
+++ lesson03/lesson3-exercise3.js
@@ -3,7 +3,7 @@ let plants = ["monsteras", "calatheas", "pothos", "ferns", "peperomias"];
 
 // Use a for loop to console log all of the elements, plus the index and a comma
 plants.forEach(function (index, item) {
-  console.log(item, ",  ", index);
+  console.log(item, ", ", index);
 });
 
 // Example:
