a175d9a640a0210f1d24620f7ec63fd1b41da00f
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2021-11-20 12:39:45 -0800

    Added lesson 2, assignment 1

---
+++ lesson02/calculator.js
@@ -0,0 +1,16 @@
+// Declare at least four variables.
+const a = 5;
+let b = 10;
+let c = 100;
+let d = b + c;
+
+
+// Do math operations on them.
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(c / b)
+console.log(a + d)
+console.log(b - a)
+console.log(c * a)
