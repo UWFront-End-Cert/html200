8a23f6615ca761f70293af445621901a1b96624c
Kennedy Caldwell <87290926+Kaldwellify@users.noreply.github.com>
2021-12-28 23:21:51 -0800

    First commit

---
+++ lesson02/calculator.js
@@ -0,0 +1,10 @@
+let w = 17;
+let x = 23;
+let y = 12;
+let z = 99;
+
+
+console.log(w + x); //outputs 40
+console.log(y - z); //outputs -87
+console.log(z / x + y); //
+console.log(w * x + y - z);
---
+++ lesson02/countdown.js
@@ -0,0 +1,3 @@
+for (let count = 20; count >= 0; count--) {
+  console.log(count);
+} // counts down from 20 stopping at 0
