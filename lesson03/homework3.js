c0ba5ea0b55ebad160717d2833aa2672181017ff
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-01-06 17:08:00 -0800

    Added challenge 3

--- lesson03/homework3.js
+++ lesson03/homework3.js
@@ -1,6 +1,7 @@
 // Create an array that has at least 5 strings:
 let animalArr = ["Hamster", "Tiger", "Crocodile", "Elephant", "Rabbit", "Monkey", "Wolf", "Parakeet"];
 
+
 // Use a for loop to console log all of the elements plus the index and a comma
 for(let i = 0; i < animalArr.length; i++) {
   console.log(i + "," + animalArr[i]);
