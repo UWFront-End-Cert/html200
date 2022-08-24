598a7de7889099ae3c0167ba2057fbd7394aab2a
Stefan Milne <stefantmilne@Stefans-MacBook-Air.local>
2022-08-24 16:35:43 -0700

    added calculator.js

---
+++ lesson02/Lesson02/calculator.js
@@ -0,0 +1,11 @@
+let x = 9;
+let y = 12;
+let z = 34;
+let w = 2;
+let v = x - w;
+
+console.log(x * y);
+console.log(z - w * (x - w));
+console.log(x % w);
+console.log(y % w);
+console.log(v / w);
