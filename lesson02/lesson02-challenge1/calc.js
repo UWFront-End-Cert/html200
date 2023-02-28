810a8bcab20bc647e846fdf5c72ec56c4e8e3327
JenPal <35875219+JPELAYO0658@users.noreply.github.com>
2023-02-28 14:07:03 -0800

    lesson02-ch1 mods

--- lesson02/lesson02-challenge1/calc.js
+++ lesson02/lesson02-challenge1/calc.js
@@ -2,9 +2,25 @@ let a = 1;
 let b = 2;
 let c = 3;
 let d = 4;
+let e = 100;
 
-console.log (1 + 2 + 3 + 4);
-console.log (1 * 2 * 3 * 4);
-console.log (4 % 3);
-console.log (((4 * 3) * 2) + 1);
-console.log ((3 * (2 * 3 + 4) + 4));
+
+console.log (a + b + c + d);
+console.log (a * b * c * d);
+console.log (d % c);
+console.log (((d * c) * b) + a);
+console.log ((c * (b * c + d) + d));
+
+if (e / 2 >= 50) {
+  console.log('The answer is equal to 50');
+}
+
+if (e / 4 === 26) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+
+for (let i = 0; i <= 3; i++) {
+    console.log(a);
+}
