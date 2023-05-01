f502a9a487a7bd0ae6126b839f78c61edd676993
JenPal <35875219+JPELAYO0658@users.noreply.github.com>
2023-05-01 13:14:50 -0700

    scripts folder

---
+++ adopt-a-dog/profiles_cart.js
@@ -0,0 +1,13 @@
+function cartTotal() {
+    let quantity = prompt('Enter the quantiy of dogs you are planning to adopt: ');
+    let fee = 123.45;
+    let cartTotal = 0;
+    
+    cartTotal = fee * quantity;
+
+    alert ('Your total fees are: $' + (cartTotal));
+}
+
+
+
+
