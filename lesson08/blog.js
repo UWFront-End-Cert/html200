9f203985b20ec1c42a75a281cd5bf70730bc3988
JenPal <35875219+JPELAYO0658@users.noreply.github.com>
2023-06-13 12:41:59 -0700

    lesson 08 - dynamic js files

---
+++ lesson08/blog.js
@@ -0,0 +1,59 @@
+let div1 = document.createElement('div');
+let title1 = document.createElement('h2');
+title1.textContent = 'Travelling with Your Dog'; 
+title1.style.textAlign = 'Center';
+
+let img1 = document.createElement('img');
+img1.src = '../adopt-a-dog/images/blog/dog-owner-mountain.jpg';
+img1.style.width = '30%';
+img1.style.height = '30%';
+img1.style.float = 'left';
+img1.style.padding = '1em';
+
+let p1 = document.createElement('p');
+p1.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore,undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat. Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim';
+
+let flexbox = document.querySelector('.flexbox-container');
+flexbox.appendChild(div1);
+div1.appendChild(title1);
+div1.appendChild(img1);
+div1.appendChild(p1);
+
+let div2 = document.createElement('div');
+let title2 = document.createElement('h2');
+title2.textContent = 'How to Walk Multiple Dogs';
+title2.style.textAlign = 'Center';
+
+let img2 = document.createElement('img');
+img2.src = '../adopt-a-dog/images/blog/dogs-in-lineup.jpg';
+img2.style.width = '30%';
+img2.style.height = '30%';
+img2.style.float = 'left';
+img2.style.padding = '1em';
+
+let p2 = document.createElement('p');
+p2.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore,undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat. Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim';
+
+flexbox.appendChild(div2);
+div2.appendChild(title2);
+div2.appendChild(img2);
+div2.appendChild(p2);
+
+let div3 = document.createElement('div');
+let title3 = document.createElement('h2');
+title3.style.textAlign = 'Center';
+title3.textContent = 'Teach Your Dog to Fetch!';
+let img3 = document.createElement('img');
+img3.src = ('../adopt-a-dog/images/blog/dog-playing-catch.jpg');
+img3.style.width = '30%';
+img3.style.height = '30%';
+img3.style.float = 'left';
+img3.style.padding = '1em';
+
+let p3 = document.createElement('p');
+p3.textContent = 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore,undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat. Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim';
+
+flexbox.appendChild(div3);
+div3.appendChild(title3);
+div3.appendChild(img3);
+div3.appendChild(p3);
