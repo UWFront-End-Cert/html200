7844f6e1b8212bfe888f2819330ec8ce070aad3d
kurosawaftw7 <jdharman@gmail.com>
2023-08-03 14:05:17 -0700

    Update files

--- adopt-a-dog/demo.js
+++ adopt-a-dog/demo.js
@@ -1,5 +1,7 @@
 object.onclick = function dogDescription('Murphy, Breed, Adoption Cost'){
-    let dogName = 
+    const dogName = 'Murphy'
+    const dogBreed = 'Breed'
+    const dogCost = '$100.00'
 
     result = 
 
