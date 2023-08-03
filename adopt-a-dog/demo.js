9d7c75edb62e93e203cac111aef606fa3b237bcb
kurosawaftw7 <jdharman@gmail.com>
2023-08-03 13:21:57 -0700

    Update file

--- adopt-a-dog/demo.js
+++ adopt-a-dog/demo.js
@@ -1,6 +1,3 @@
-function dogDescription('Adorable happy dog.'){
-    const object.onclick =  'Adorable happy dog.'
-    const result = 0
-
-    result = dogDescription
+object.onclick = function dogDescription('Adorable happy dog.'){
+    
 }
