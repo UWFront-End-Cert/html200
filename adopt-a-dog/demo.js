0c18ae115442e09d46b92ca9f13522490d846df2
kurosawaftw7 <jdharman@gmail.com>
2023-08-08 13:18:27 -0700

    Update files

--- adopt-a-dog/demo.js
+++ adopt-a-dog/demo.js
@@ -7,3 +7,73 @@ function dogDescription(){
 
     alert(result)
 }
+
+function dogDescription(){
+    const dogName = 'Poppy'
+    const dogBreed = ' Breed'
+    const dogCost = ' $100.00'
+
+    result = dogName + dogBreed + dogCost
+
+    alert(result)
+}
+
+function dogDescription(){
+    const dogName = 'Jack'
+    const dogBreed = ' Breed'
+    const dogCost = ' $100.00'
+
+    result = dogName + dogBreed + dogCost
+
+    alert(result)
+}
+
+function dogDescription(){
+    const dogName = 'Duffy'
+    const dogBreed = ' Breed'
+    const dogCost = ' $100.00'
+
+    result = dogName + dogBreed + dogCost
+
+    alert(result)
+}
+
+function dogDescription(){
+    const dogName = 'Lucas'
+    const dogBreed = ' Breed'
+    const dogCost = ' $100.00'
+
+    result = dogName + dogBreed + dogCost
+
+    alert(result)
+}
+
+function dogDescription(){
+    const dogName = 'Jake'
+    const dogBreed = ' Breed'
+    const dogCost = ' $100.00'
+
+    result = dogName + dogBreed + dogCost
+
+    alert(result)
+}
+
+function dogDescription(){
+    const dogName = 'Angus'
+    const dogBreed = ' Breed'
+    const dogCost = ' $100.00'
+
+    result = dogName + dogBreed + dogCost
+
+    alert(result)
+}
+
+function dogDescription(){
+    const dogName = 'Violet'
+    const dogBreed = ' Breed'
+    const dogCost = ' $100.00'
+
+    result = dogName + dogBreed + dogCost
+
+    alert(result)
+}
