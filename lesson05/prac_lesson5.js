35bf1cafdc068bad0faf147eea165fef5f1dc806
kurosawaftw7 <jdharman@gmail.com>
2023-08-17 10:51:30 -0700

    Upload file

---
+++ lesson05/prac_lesson5.js
@@ -0,0 +1,9 @@
+const hairLengthInInches = 10;
+
+if (hairLengthInInches < 2) {
+    console.log('short hair');
+} else if (hairLengthInInches >= 2 && hairLengthInInches < 5) {
+    console.log('medium hair');
+} else {
+    console.log('long hair');
+}
