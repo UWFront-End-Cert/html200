2c5867666a27a510d33e513146bdc6e16f54ff6f
kurosawaftw7 <jdharman@gmail.com>
2023-08-17 19:24:50 -0700

    Update files

--- lesson05/bank_app.js
+++ lesson05/bank_app.js
@@ -1,5 +1,5 @@
 let yourBalance = 50000;
-// Created starting balance, create function to hold all of our code when the user starts, get user's input, pass input to switch statement, in switch statement, decide what needs to be done.
+
 const yourAccount = function(){
     input = prompt('What would you like to do today? Q to Quit, W to Withdraw, D to Deposit, and B to View Balance');
     switch(input){
