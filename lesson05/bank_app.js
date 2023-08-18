4992a73b0a1512f5b2e1a3a34499b7ecc97134a9
kurosawaftw7 <jdharman@gmail.com>
2023-08-17 19:23:21 -0700

    Update files

--- lesson05/bank_app.js
+++ lesson05/bank_app.js
@@ -3,21 +3,23 @@ let yourBalance = 50000;
 const yourAccount = function(){
     input = prompt('What would you like to do today? Q to Quit, W to Withdraw, D to Deposit, and B to View Balance');
     switch(input){
-        case input = 'B', 'b':
+        case input = 'B':
             alert(yourBalance);
             break;
-        case input = 'Q', 'q':
+        case input = 'Q':
             alert('Goodbye');
             break;
-        case input = 'D', 'd':
+        case input = 'D':
             depositInput = prompt('Enter your deposit here:');
             yourBalance = parseInt(depositInput) + yourBalance;
             alert(yourBalance);
             break;
-        case input = 'W', 'w':
+        case input = 'W':
             withdrawInput = prompt('Enter your withdrawal here:');
-            if (yourBalance = <=0);
             yourBalance = yourBalance - parseInt(withdrawInput);
+            if (yourBalance <=0){
+                alert('Warning: Approaching Zero');
+            }
             alert(yourBalance);
             break;
     }
