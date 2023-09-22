8e9fac06afeb6fcc15b9ded0fc853950d83c246e
kurosawaftw7 <jdharman@gmail.com>
2023-09-22 16:53:23 -0700

    Update files

---
+++ main.js
@@ -0,0 +1,13 @@
+const fields = [
+    {
+        name: 'full-name',
+        label: 'Full name'
+    },
+    {
+        name: 'email',
+        label: 'Email address'
+    }
+];
+
+const form = document.createElement('form');
+document.body.appendChild(form);
