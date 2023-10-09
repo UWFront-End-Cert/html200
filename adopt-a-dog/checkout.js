bbe73dc41e06b872e8a05e44700d61748e49b5ae
kurosawaftw7 <jdharman@gmail.com>
2023-10-09 16:29:03 -0700

    Update files

--- adopt-a-dog/checkout.js
+++ adopt-a-dog/checkout.js
@@ -1,4 +1,5 @@
 function submitButton() {
-    let input = prompt('Thank you. The form information has been received.')
+    let input = prompt('Thank you. The form information has been received.');
+
     alert(result);
 };
