f98dbf50abb1a9560161584c7e1372ad5d655ba5
kurosawaftw7 <jdharman@gmail.com>
2023-10-12 19:20:52 -0700

    Update files

--- adopt-a-dog/checkout.js
+++ adopt-a-dog/checkout.js
@@ -1,5 +1,66 @@
-function submitButton(result) {
-    result = document.write("Thank you. The form information has been received.");
+const fields = [
+    {
+        name: 'name',
+        label: 'Name'
+    },
+    {
+        name: 'address',
+        label: 'Address'
+    },
+    {
+        name: 'city',
+        label: 'City'
+    },
+    {
+        name: 'state',
+        label: 'State'
+    },
+    {
+        name: 'zip',
+        label: 'Zip'
+    },
+    {
+        name: 'ownerlevel',
+        label: 'First Time Adopter?'
+    },
+    {
+        name: 'pickuplocation',
+        label: 'Pickup Location'
+    },
+]
 
-    alert(result);
+const dogform = document.createElement('form');
+
+const checkoutTitle = document.createElement('h1');
+checkoutTitle.textContent = 'Checkout';
+
+let checkoutForm = document.querySelector('.checkout');
+checkoutForm.appendChild(checkoutTitle);
+checkoutForm.appendChild(dogform);
+
+
+
+
+for (let i = 0; i < fields.length; i++) {
+    const field = fields[i];
+
+    const label = document.createElement('label');
+    label.textContent = field.label;
+    label.setAttribute('for', 'field.name');
+
+    const input = document.createElement('input');
+    input.setAttribute('id', 'field.name');
+
+    dogform.appendChild(label);
+    dogform.appendChild(input);
 }
+
+const submitButton = document.createElement('button');
+submitButton.setAttribute('type', 'submit');
+submitButton.textContent = 'Submit';
+dogform.appendChild(submitButton);
+
+dogform.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you. The form information has been received.")
+});
