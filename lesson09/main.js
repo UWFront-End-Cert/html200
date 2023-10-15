bab1e0e068adc51dd7fd4fa251a298012636d755
kurosawaftw7 <jdharman@gmail.com>
2023-10-14 18:04:08 -0700

    Update files

--- lesson09/main.js
+++ lesson09/main.js
@@ -8,4 +8,12 @@ $(function(){
     $('show').click(function(){
         $('img').show()
     })
+
+    $('img').mouseenter(function(){
+        console.log("mouse has entered")
+        $(this).fadeTo('slow', .75)
+    }).mouseleave(function(){
+        console.log("mouse has left")
+        $(this).fadeTo('fast', 1)
+    })
 })
